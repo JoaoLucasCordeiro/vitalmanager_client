8933b12d6e1959169e24dd681c674c579edb1510
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2024-07-22 19:42:14 -0300

    fix: fix exam title

--- src/routes/ExamsMed.jsx
+++ src/routes/ExamsMed.jsx
@@ -46,35 +46,37 @@ const ExamsMed = () => {
     const parts = dateString.split("-"); // Separa a data em [ano, mês, dia]
     return `${parts[2]}/${parts[1]}/${parts[0]}`; // Reorganiza para DD/MM/YYYY
   }
-  
+
   // No componente ExamsMed, ao renderizar CardExam:
-  {exams.map((exam) => (
-    <CardExam
-      key={exam.idExame}
-      dataExame={formatDate(exam.dataExame)} // Formata a data antes de passar
-      tipoExame={exam.tipoExame}
-    />
-  ))}
+  {
+    exams.map((exam) => (
+      <CardExam
+        key={exam.idExame}
+        dataExame={formatDate(exam.dataExame)} // Formata a data antes de passar
+        tipoExame={exam.tipoExame}
+      />
+    ));
+  }
 
   return (
     <section className="flex flex-col sm:justify-normal justify-center md:items-baseline items-center h-screen p-4 md:ml-[12%] ml-0">
       <div>
         <img src="/loggedlogo.png" alt="VitalManager Logo" />
         <h2 className="text-purple-400 font-bold text-3xl text-center md:text-left py-5">
-          Exames
+          Exames marcados
         </h2>
 
         <div className="flex flex-col items-center md:justify-normal justify-center sm:mt-10 mt-5 md:space-x-4 space-x-0 sm:flex-row sm:gap-0 gap-4">
           <Searcher placeholder="Procurar exame" />
         </div>
         <div className="ml-5 flex items-center flex-wrap gap-3 py-5">
-        {exams.map((exam) => (
-  <CardExam
-    key={exam.idExame}
-    dataExame={formatDate(exam.dataExame)} // Formata a data antes de passar
-    tipoExame={exam.tipoExame}
-  />
-))}
+          {exams.map((exam) => (
+            <CardExam
+              key={exam.idExame}
+              dataExame={formatDate(exam.dataExame)} // Formata a data antes de passar
+              tipoExame={exam.tipoExame}
+            />
+          ))}
         </div>
       </div>
     </section>
